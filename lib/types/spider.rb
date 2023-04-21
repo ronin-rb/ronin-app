0440e8e8af699c40385c93afb1edf06208f5c639
Postmodern <postmodern.mod3@gmail.com>
2023-04-21 16:55:08 -0700

    Require `types` for `Types::String`.

--- lib/types/spider.rb
+++ lib/types/spider.rb
@@ -1,9 +1,7 @@
-require 'dry-types'
+require 'types'
 
 module Types
   module Spider
-    include Dry::Types()
-
     # The type of web spider targets
     TargetType = Types::String.enum('host', 'domain', 'site')
   end
