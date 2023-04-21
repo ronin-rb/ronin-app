2e892ed8152d418a5a1ad2068063874a869dd831
Postmodern <postmodern.mod3@gmail.com>
2023-04-21 16:55:53 -0700

    Added specs for `Workers::Spider::Params`.

---
+++ spec/workers/spider_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'workers/spider'
+
+describe Workers::Spider do
+  describe "Params" do
+    subject { described_class::Params }
+
+    it "must require a :type key" do
+      result = subject.call({target: 'example.com'})
+
+      expect(result).to be_failure
+      expect(result.errors[:type]).to eq(["is missing"])
+    end
+
+    it "must require a :target key" do
+      result = subject.call({type: 'host'})
+
+      expect(result).to be_failure
+      expect(result.errors[:target]).to eq(["is missing"])
+    end
+  end
+end
