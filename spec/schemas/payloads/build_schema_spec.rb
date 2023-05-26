dcb9158c470a9ad68eaacb319818d5a207a352d0
Postmodern <postmodern.mod3@gmail.com>
2023-05-26 14:01:04 -0700

    Add missing specs for `Schemas::Payloads::BuildSchema()`.

---
+++ spec/schemas/payloads/build_schema_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require 'schemas/payloads/build_schema'
+
+require 'ronin/payloads/payload'
+
+describe "Schemas::Payloads::BuildSchema" do
+  context "when the given payload class has no params" do
+    module TestBuildSchema
+      class PayloadWithoutParams < Ronin::Payloads::Payload
+      end
+    end
+
+    let(:payload_class) { TestBuildSchema::PayloadWithoutParams }
+
+    subject { Schemas::Payloads::BuildSchema(payload_class) }
+
+    it "must build an empty schema" do
+      expect(subject.call({})).to be_success
+    end
+  end
+
+  context "when the given payload class has params" do
+    module TestBuildSchema
+      class PayloadWithParams < Ronin::Payloads::Payload
+
+        param :foo, Integer, required: true,
+                             desc:     'A required param'
+
+        param :bar, String, desc: 'An optional param'
+
+      end
+    end
+
+    let(:payload_class) { TestBuildSchema::PayloadWithParams }
+
+    subject { Schemas::Payloads::BuildSchema(payload_class) }
+
+    it "must build an empty schema" do
+      result = subject.call({params: {}})
+
+      expect(result).to be_failure
+      expect(result.errors[:params][:foo]).to eq(['is missing'])
+    end
+  end
+end
