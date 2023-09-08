dabf9ed6e66537c1b38bc2aa5f917df310166315
Postmodern <postmodern.mod3@gmail.com>
2023-09-08 12:48:45 -0700

    Also support using the `DATABASE_NAME` env variable (closes #51).

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 require 'ronin/db'
 
-Ronin::DB.connect(ENV['DATABASE_URL'] || :default)
+database = if    ENV['DATABASE_URL']  then ENV['DATABASE_URL']
+           elsif ENV['DATABASE_NAME'] then ENV['DATABASE_NAME'].to_sym
+           else                            :default
+           end
+
+Ronin::DB.connect(database)
