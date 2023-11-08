347c4cb8d458081e9ea1e8754f7e3131fe3d0344
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-08 15:07:16 +0100

    Add route for inspecting sidekiq jobs (#63)

--- app.rb
+++ app.rb
@@ -64,6 +64,7 @@ require './workers/spider'
 require './workers/recon'
 
 require 'ronin/app/version'
+require 'sidekiq/api'
 
 #
 # Main app class.
@@ -770,4 +771,19 @@ class App < Sinatra::Base
     erb :about
   end
 
+  get '/queue' do
+    @workers = Sidekiq::Workers.new.map do |_process_id, _thread_id, worker|
+      payload = JSON.parse(worker["payload"])
+      {
+        queue:       worker["queue"],
+        class:       payload["class"],
+        args:        payload["args"],
+        created_at:  Time.at(payload["created_at"]),
+        enqueued_at: Time.at(payload["enqueued_at"]),
+        run_at:      Time.at(worker["run_at"])
+      }
+    end
+
+    erb :queue
+  end
 end
