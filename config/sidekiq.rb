6143bb873a40efff122e7ce9daaeedbd42e3f478
Postmodern <postmodern.mod3@gmail.com>
2024-05-01 14:23:03 -0700

    Add Sidekiq middleware to clear the ActiveRecord connection pool (issue #16).

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -2,12 +2,18 @@
 require 'sidekiq'
 require 'redis/namespace'
 
+require_relative '../lib/middleware/sidekiq/active_record_connection_pool'
+
 redis_config = {
   url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}"
 }
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
+
+  config.server_middleware do |chain|
+    chain.add Middleware::Sidekiq::ActiveRecordConnectionPool
+  end
 end
 
 Sidekiq.configure_client do |config|
